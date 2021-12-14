359c43d385aa2897366bc0e013cf29faa099f111
p p <pvlskn@yandex.ru>
2021-12-14 09:42:26 +0300

    Добавьте файлы проекта.

---
+++ HW8 -Create text/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+/*Программно создайте текстовый файл и запишите в него 10 случайных чисел. 
+ * Затем программно откройте созданный файл, рассчитайте сумму чисел в нем, ответ выведите на консоль.*/
+class Program
+{
+    static void Main(string[] args)
+    {
+                int[] numbers = new int[10];
+        Random random = new Random();
+        for (int i = 0; i < 10; i++)
+        {
+            numbers[i] = random.Next(0, 100);
+        }
+        string Path = "RND_numbers.txt";
+        StreamWriter TxtFile = new StreamWriter(Path, false);
+        Array.ForEach(numbers, Console.WriteLine);
+        TxtFile.Close();
+        /* StreamWriter TxtFile = new StreamWriter(Path, false);
+         TxtFile.WriteLine(numbers[]);
+         TxtFile.Close();*/
+
+        Console.WriteLine("Нажмите любую клавишу для выхода.");
+        Console.ReadKey();
+    }
+}
---
+++ HW8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+//Выберите любую папку на своем компьютере, имеющую вложенные директории. Выведите на консоль ее содержимое и содержимое всех подкаталогов.
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string Path = @"C:\Users";
+        if (Directory.Exists(Path))
+        {
+            Console.WriteLine("Содержимое подкаталогов {0}", Path);
+            string[] Cat = Directory.GetDirectories(Path);
+            foreach (string c in Cat)
+            {
+                Directory.GetDirectories(Path);
+                Console.WriteLine(c);
+                string[] subCat = Directory.GetDirectories(c);
+                foreach (string sc in subCat)
+                {
+                    Directory.GetDirectories(c);
+                    Console.WriteLine(sc);
+                }
+                Console.WriteLine();
+            }
+        }
+        Console.WriteLine("Нажмите любую клавишу для выхода.");
+        Console.ReadKey();
+    }
+}
